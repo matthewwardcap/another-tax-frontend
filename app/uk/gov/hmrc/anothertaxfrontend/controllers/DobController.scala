da59f8c191277aa60633b90bab4082b98e97aad7
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2022-02-18 10:45:45 +0000

    Date parse working correctly

--- app/uk/gov/hmrc/anothertaxfrontend/controllers/DobController.scala
+++ app/uk/gov/hmrc/anothertaxfrontend/controllers/DobController.scala
@@ -37,7 +37,6 @@ class DobController @Inject()(
   }
 
   def post: Action[AnyContent] = Action.async { implicit request =>
-    //val user: User = Json.toJson(request.session.get("user")).as[User]
     val user = request.session.get("user").map(user => Json.parse(user).as[User])
     val format = new java.text.SimpleDateFormat("dd-MM-yyyy")
     form
@@ -47,7 +46,7 @@ class DobController @Inject()(
         dataForm => Future.successful(Redirect(uk.gov.hmrc.anothertaxfrontend.controllers.routes.EduBoolController.show)
           .addingToSession(
             "user" -> Json.toJson(user.map(us => us.copy(
-              dob = Option(format.parse(dataForm.day.toString+dataForm.month.toString+dataForm.year.toString))
+              dob = Option(format.parse(dataForm.day.toString+"-"+dataForm.month.toString+"-"+dataForm.year.toString))
             ))).toString
           )
         )
