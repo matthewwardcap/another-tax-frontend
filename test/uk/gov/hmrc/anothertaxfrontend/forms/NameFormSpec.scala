a3e967ab46ec89d2577738b1ae3fff7dbcbdf89c
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2022-03-02 13:24:51 +0000

    NameFormSpec

---
+++ test/uk/gov/hmrc/anothertaxfrontend/forms/NameFormSpec.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.anothertaxfrontend.forms
+
+import play.api.data.Form
+import uk.gov.hmrc.anothertaxfrontend.core.UnitSpec
+
+class NameFormSpec extends UnitSpec {
+  val form: Form[Data] = NameForm.form
+
+  "NameForm.form" when {
+
+    "no first name is supplied" must {
+      "result in a an error against the first name field" in {
+        val result = form.bind(Map("lastName" -> "Smith"))
+        val error = result("firstName").error.getOrElse(fail("error against first name field not generated"))
+        error.message mustBe "error.required"
+      }
+    }
+
+    "no last name is supplied" must {
+      "result in a an error against the last name field" in {
+        val result = form.bind(Map("firstName" -> "Smith"))
+        val error = result("lastName").error.getOrElse(fail("error against first name field not generated"))
+        error.message mustBe "error.required"
+      }
+    }
+
+    "bound with data containing a valid name without a middle name" must {
+
+      val model = Data("John", None, "Smith")
+      val data = Map("firstName" -> model.firstName, "lastName" -> model.lastName)
+
+      "result in no errors" in {
+        val result = form.bind(data)
+        val errors = result.hasErrors
+        errors mustBe false
+      }
+
+      "generate a FullName model with no middle name" in {
+        val result = form.bind(data)
+        if (result.hasErrors) {
+          fail("errors reported on a valid form")
+        } else {
+          result.value mustBe Some(model)
+        }
+      }
+    }
+
+    "bound with data containing a valid name with a middle name" must {
+
+      val model = Data("John", Some("Middle"), "Smith")
+      val data = Map(
+        "firstName" -> model.firstName,
+        "middleName" -> model.middleName.get,
+        "lastName" -> model.lastName
+      )
+
+      "result in no errors" in {
+        val result = form.bind(data)
+        val errors = result.hasErrors
+        errors mustBe false
+      }
+
+      "generate a FullName model with no middle name" in {
+        val result = form.bind(data)
+        if (result.hasErrors) {
+          fail("errors reported on a valid form")
+        } else {
+          result.value mustBe Some(model)
+        }
+      }
+    }
+
+    "built with a model containing a middle name" must {
+
+      val model = Data("John", Some("Middle"), "Smith")
+      val data = Map(
+        "firstName" -> model.firstName,
+        "middleName" -> model.middleName.get,
+        "lastName" -> model.lastName
+      )
+
+      "result in a form with no errors" in {
+        val result = form.fillAndValidate(model)
+        val errors = result.hasErrors
+        errors mustBe false
+      }
+
+      "generate a populated form" in {
+        val result = form.fillAndValidate(model)
+        result.data mustBe data
+      }
+    }
+
+    "built with a model containing no middle name" must {
+
+      val model = Data("John", None, "Smith")
+      val data = Map(
+        "firstName" -> model.firstName,
+        "lastName" -> model.lastName
+      )
+
+      "result in a form with no errors" in {
+        val result = form.fillAndValidate(model)
+        val errors = result.hasErrors
+        errors mustBe false
+      }
+
+      "generate a populated form" in {
+        val result = form.fillAndValidate(model)
+        result.data mustBe data
+      }
+    }
+
+  }
+}
